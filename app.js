e89d026e1231a3c8cf3b041a404a50bc85aa9bdb
VitorJorgeOficial <vtr.jorge1@gmail.com>
2024-09-11 00:25:33 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+   
+    let campoPesquisa = document.getElementById("campo-pesquisa").value 
+    
+    // se campoPesquisa for uma string sem nada
+
+    if(!campoPesquisa ) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome do filme ou a Categoria</p>"
+        return
+    }
+    
+    campoPesquisa = campoPesquisa.toLowerCase()
+  
+    // Inicializa uma string vazia para armazenar o HTML dos resultados
+    let resultados = "";
+    let titulo =  "";
+    let categoria = "";
+    let tags = "";
+  
+    // Itera sobre cada item de dados e constrói o HTML correspondente
+    for (let dado of dados) {
+      titulo = dado.titulo.toLowerCase()
+      categoria = dado.categoria.toLowerCase()
+      tags = dado.tags.toLocaleLowerCase()
+        // se titulo includes campoPesquisa
+        if(titulo.includes(campoPesquisa) || categoria.includes(campoPesquisa)|| tags.includes(campoPesquisa)) {
+          // Cria um novo elemento
+
+          resultados += `
+<div class="item-resultado">
+  <h2>
+    <a href="#" target="_blank">${dado.titulo}</a>
+  </h2>
+  <p class="descricao-meta">${dado.categoria}</p>
+  <p class="sinopse-meta">${dado.sinopse}</p>
+  <a href="${dado.link}" target="_blank">Mais informações</a>
+</div>
+`;
+// Cria um novo elemento de resultado com as informações do dado
+
+        }
+           
+      
+    }
+    if(!resultados ){
+      resultados = "<p>Nada foi encontrado</p>"
+
+    }
+  
+    // Atualiza o conteúdo HTML da seção de resultados com os resultados gerados
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,91 @@
+let dados = [
+    {
+        titulo : "O Senhor Dos Aneis",
+        sinopse : "O Senhor dos Anéis é uma trilogia cinematográfica dirigida por Peter Jackson com base na obra-prima homónima de J. R. R. Tolkien.",
+        link : "https://pt.wikipedia.org/wiki/O_Senhor_dos_An%C3%A9is_(s%C3%A9rie_de_filmes)",
+        categoria : "Literatura fantástica, Alta fantasia, Ficção de aventura, Romance de cavalaria, Fantasia heroica",
+        tags: "aneis senhor frodo"
+
+    },
+    
+    {
+        titulo : "Harry Potter",
+        sinopse : "Harry Potter é um jovem comum de 11 anos até que recebe uma carta para estudar na Escola de Magia e Bruxaria de Hogwarts, onde ele descobre ser, na verdade, um bruxo. Nela, o garoto conquista a amizade de Hagrid, além de Hermione e Ron. No entanto, ele descobre ter um  grande inimigo, o terrível Voldemort.",
+        link : "https://pt.wikipedia.org/wiki/Harry_Potter",
+        categoria : "ficção fantástica",
+        tags:"bruxo bruxa Hogwarts bruxaria"
+    },
+        
+        
+    {
+        titulo: "Matrix",
+        sinopse :"O jovem programador Thomas Anderson é atormentado por estranhos pesadelos em que está sempre conectado por cabos a um imenso sistema de computadores do futuro. À medida que o sonho se repete, ele começa a desconfiar da realidade. Thomas conhece os misteriosos Morpheus e Trinity e descobre que é vítima de um sistema inteligente e artificial chamado Matrix, que manipula a mente das pessoas e cria a ilusão de um mundo real enquanto usa os cérebros e corpos dos indivíduos para produzir energia.",
+        link : "https://pt.wikipedia.org/wiki/Matrix",
+        categoria : "Ação/Ficção científica",
+        tags:"programador Trinity Morpheus sistema maquinas"
+    },
+
+    {
+        titulo:"As Crônicas de Nárnia",
+        sinopse :"Durante os bombardeios da Segunda Guerra Mundial de Londres, quatro irmãos ingleses são enviados para uma casa de campo onde eles estarão seguros. Um dia, Lucy encontra um guarda-roupa que a transporta para um mundo mágico chamado Nárnia. Depois de voltar, ela logo volta a Nárnia com seus irmãos, Peter e Edmund, e sua irmã, Susan. Lá eles se juntam ao leão mágico, Aslan, na luta contra a Feiticeira Branca.",
+        link : "https://pt.wikipedia.org/wiki/The_Chronicles_of_Narnia",
+        categoria : "Fantasia/Aventura",
+        tags:"leão feiticeira guarda-roupas Nárnia narnia Aslan"
+    },
+    
+    {
+        titulo:"O Hobbit", 
+        sinopse :"Como a maioria dos hobbits, Bilbo Bolseiro leva uma vida tranquila até o dia em que recebe uma missão do mago Gandalf. Acompanhado por um grupo de anões, ele parte numa jornada até a Montanha Solitária para libertar o Reino de Erebor do dragão Smaug.",
+        link : "https://pt.wikipedia.org/wiki/O_Hobbit",
+        categoria : "Romance, Literatura fantástica, Alta fantasia, Literatura infantil, Épico",
+        tags:"anão Smaug hobbits Bilbo"
+    },
+
+    {
+       titulo:"Interestelar",
+       sinopse: "As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial, possibilitando a continuação da espécie. Cooper é chamado para liderar o grupo e aceita a missão sabendo que pode nunca mais ver os filhos. Ao lado de Brand, Jenkins e Doyle, ele seguirá em busca de um novo lar.",
+       link : "https://pt.wikipedia.org/wiki/Interstellar",
+       categoria : "Ficção científica/Aventura",
+       tags:"astronauta Cooper viagem no tempo "
+    },
+
+   {
+       titulo:"Cruzada",
+       sinopse: "Ainda em luto pela repentina morte de sua esposa, o ferreiro Balian junta-se ao seu distante pai, Baron Godfrey, nas cruzadas a caminho de Jerusalém. Após uma jornada muito difícil até à cidade santa, o jovem valente entra no séquito do rei leproso Balduíno IV, que deseja lutar contra os muçulmanos para seu próprio ganho político e pessoal.",
+       link : "https://pt.wikipedia.org/wiki/Kingdom_of_Heaven",
+       categoria : "Aventura/Ação",
+       tags:"Godfrey Jerusalém saladino Balduíno"
+   },
+   {
+       titulo:"O Rei Leão",
+       sinopse: "Este desenho animado da Disney mostra as aventuras de um leão jovem de nome Simba, o herdeiro de seu pai, Mufasa. O tio malvado de Simba, Oscar, planeja roubar o trono de Mufasa atraindo pai e filho para uma emboscada. Simba consegue escapar e somente Mufasa morre. Com a ajuda de seus amigos,Timon e Pumba, ele reaparece como adulto para recuperar sua terra, que foi roubada por seu tio Oscar.",
+       link : "https://pt.wikipedia.org/wiki/O_Rei_Le%C3%A3o",
+       categoria : " Infantil/Musical",
+       tags:"leão Rei Disney Simba Mufasa Oscar"
+   },
+   {
+       titulo:"Maze Runner",
+       sinopse : "Em um futuro apocalíptico, o jovem Thomas é escolhido para enfrentar o sistema. Ele acorda dentro de um escuro elevador em movimento e não consegue se lembrar nem de seu nome. Na comunidade isolada em que foi abandonado, Thomas conhece outros garotos que passaram pela mesma situação. Para conseguir escapar, ele precisa descobrir os sombrios segredos guardados em sua mente e correr muito.",
+       link : "https://pt.wikipedia.org/wiki/Maze_Runner_-_Correr_ou_Morrer",
+       categoria : "Ficção científica/Ação",
+       tags:"apocalíptico labirinto correr"
+   },
+   {
+       titulo:"Guerra Dos Mundos",
+       sinopse : "Ray Ferrier é um estivador divorciado, afastado de seus filhos, que vivem com a mãe. Quando sua ex-esposa deixa as crianças para ele cuidar durante alguns dias, o planeta é atacado por alienígenas que surgem do chão conduzindo tripods e destruindo tudo que encontram pelo caminho. Ray tenta proteger os seus filhos e fugir para Boston para se juntar a sua ex-esposa. Durante o trajeto, enfrenta várias adversidades e ataques.",
+       link : "https://pt.wikipedia.org/wiki/A_Guerra_dos_Mundos",
+       categoria : "Ficção científica/Ação",
+       tags:"invasão alienígena ataque"
+   },
+    
+   {
+       titulo:"Planeta Dos Macacos",
+       sinopse : "é uma franquia de mídia de ficção científica americana que consiste em filmes, livros, séries de televisão e outras mídias sobre um mundo onde seres humanos e macacos inteligentes se confrontam.",
+       link : "https://pt.wikipedia.org/wiki/Planeta_dos_Macacos",
+       categoria : "Ficção científica/Aventura",
+       tags:"macaco planeta"
+   }
+    
+    
+];
+
